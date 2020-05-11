fc05c8f3a9656ec0eec8902ece9ddd6f700bf494
JmDeleon2000 <dele19817@uvg.edu.gt>
2020-05-10 23:14:34 -0600

    entrega 10/05/2020

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.lab4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lab4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binds: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/lab4/Results.kt
+++ app/src/main/java/com/example/lab4/Results.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.lab4.databinding.FragmentResultsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +15,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeScreenFragment.newInstance] factory method to
+ * Use the [Results.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeScreenFragment : Fragment() {
+class Results : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -33,8 +35,8 @@ class HomeScreenFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+        val binding = DataBindingUtil.inflate<FragmentResultsBinding>(inflater, R.layout.fragment_results, container, false)
+        return binding.root
     }
 
     companion object {
@@ -44,12 +46,12 @@ class HomeScreenFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeScreenFragment.
+         * @return A new instance of fragment Results.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeScreenFragment().apply {
+            Results().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
